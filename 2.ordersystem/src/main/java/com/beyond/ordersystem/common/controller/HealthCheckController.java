c83fd5d1684d33bcff8b61396941b0b55ce36c4c
yune22 <wjddbs020604@gmail.com>
2026-03-12 16:49:22 +0900

    commit

--- 2.ordersystem/src/main/java/com/beyond/ordersystem/common/controller/HealthCheckController.java
+++ 2.ordersystem/src/main/java/com/beyond/ordersystem/common/controller/HealthCheckController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheckController {
     @GetMapping("/health")
     public String healthCheck() {
-        return "OK2";
+        return "OK3";
     }
 }
